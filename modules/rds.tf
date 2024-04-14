f14b6e1a984830521469f41239bb787816c404ca
amancharla <amancharla@gmail.com>
2024-04-13 20:38:50 -0700

    Update rds.tf

--- modules/rds.tf
+++ modules/rds.tf
@@ -10,31 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_db_instance" "mydb" {
-  identifier_prefix   = "mydb-1"
-  engine              = "mysql"
-  allocated_storage   = 10
-  instance_class      = var.instance_class
-  db_name             = var.db_name
-  username            = var.username
-  password            = var.password
-  skip_final_snapshot = true
-}terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
 }
 
 resource "aws_db_instance" "mydb" {
